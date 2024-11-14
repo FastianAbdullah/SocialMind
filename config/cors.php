da3c248db6b9f85beb6f8fee8a196b2501adbb35
M.Abdullah <93811691+FastianAbdullah@users.noreply.github.com>
2024-11-14 19:46:47 +0500

    changes in headers

--- config/cors.php
+++ config/cors.php
@@ -19,8 +19,6 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
-
     'allowed_origins_patterns' => [],
 
     'allowed_headers' => ['*'],
@@ -29,7 +27,7 @@ return [
 
     'max_age' => 0,
 
-    'allowed_origins' => ['https://socialmind.discountroof.co.uk'],
+    'allowed_origins' => ['http://localhost:5173','https://socialmind.discountroof.co.uk'],
 
     'supports_credentials' => false,
 
--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,24 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
-export default defineConfig({
-  plugins: [
-    laravel({
-      input: [
-        'resources/js/app.js',
-        'resources/js/legacy/plugins.js',
-        'resources/js/legacy/app.js',
-        'resources/js/legacy/animatedt-title.js',
-      ],
-      refresh: true,
-    }),
-    vue(),
-  ],
+export default defineConfig(({ mode }) => {
+  const publicPath = mode === 'production'
+    ? 'https://socialmind.discountroof.co.uk'
+    : '/';
+
+  return {
+    plugins: [
+      laravel({
+        input: [
+          'resources/js/app.js',
+          'resources/js/legacy/plugins.js',
+          'resources/js/legacy/app.js',
+          'resources/js/legacy/animatedt-title.js',
+        ],
+        refresh: true,
+      }),
+      vue(),
+    ],
+    base: publicPath,
+};
 });
