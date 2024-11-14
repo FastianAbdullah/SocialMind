9c3acd25c70d32e3853f138aebdaccd8afb515ea
M.Abdullah <93811691+FastianAbdullah@users.noreply.github.com>
2024-11-14 19:11:22 +0500

    php cors updated

--- config/cors.php
+++ config/cors.php
@@ -29,7 +29,7 @@ return [
 
     'max_age' => 0,
 
-    'allowed_origins' => ['http://localhost:5173','https://socialmind.discountroof.co.uk'],
+    'allowed_origins' => ['https://socialmind.discountroof.co.uk'],
 
     'supports_credentials' => false,
 
