b97385a8a452388c7189ac0c051117f28bb21d51
M.Abdullah <93811691+FastianAbdullah@users.noreply.github.com>
2024-11-18 20:43:03 +0500

    Privacy and Terms Updated.
    policies, terms and conditions are made according to our website.

--- resources/js/components/Privacy.vue
+++ resources/js/components/Privacy.vue
@@ -40,7 +40,7 @@
                     <span class="flex-shrink-0 d-grid place-content-center w-6 h-6 rounded-circle border border-primary-key border-opacity-3 bg-primary-key bg-opacity-2 clr-primary-key">
                       <i class="bi bi-check2"></i>
                     </span>
-                    <span class="d-block clr-neutral-70"> Intelligent image editing </span>
+                    <span class="d-block clr-neutral-70"> AutoPoster on various platforms </span>
                   </div>
                 </li>
                 <li class="welcome-list-1">
@@ -48,7 +48,7 @@
                     <span class="flex-shrink-0 d-grid place-content-center w-6 h-6 rounded-circle border border-primary-key border-opacity-3 bg-primary-key bg-opacity-2 clr-primary-key">
                       <i class="bi bi-check2"></i>
                     </span>
-                    <span class="d-block clr-neutral-70"> Customizable image styles </span>
+                    <span class="d-block clr-neutral-70"> Content Schedular </span>
                   </div>
                 </li>
                 <li class="welcome-list-1">
@@ -56,29 +56,51 @@
                     <span class="flex-shrink-0 d-grid place-content-center w-6 h-6 rounded-circle border border-primary-key border-opacity-3 bg-primary-key bg-opacity-2 clr-primary-key">
                       <i class="bi bi-check2"></i>
                     </span>
-                    <span class="d-block clr-neutral-70"> Unprecedented Image Creation </span>
+                    <span class="d-block clr-neutral-70"> Content Optimization </span>
+                  </div>
+                </li>
+                <li class="welcome-list-1">
+                  <div class="d-flex gap-3">
+                    <span class="flex-shrink-0 d-grid place-content-center w-6 h-6 rounded-circle border border-primary-key border-opacity-3 bg-primary-key bg-opacity-2 clr-primary-key">
+                      <i class="bi bi-check2"></i>
+                    </span>
+                    <span class="d-block clr-neutral-70"> Content Optimization </span>
+                  </div>
+                </li>
+                <li class="welcome-list-1">
+                  <div class="d-flex gap-3">
+                    <span class="flex-shrink-0 d-grid place-content-center w-6 h-6 rounded-circle border border-primary-key border-opacity-3 bg-primary-key bg-opacity-2 clr-primary-key">
+                      <i class="bi bi-check2"></i>
+                    </span>
+                    <span class="d-block clr-neutral-70"> Sentinment Analysis </span>
                   </div>
                 </li>
               </ul>
-              <p class="clr-neutral-80"> By accessing this website we assume you accept these terms and conditions in full. Do not continue to use Kohost's website if you do not accept all of the terms and conditions stated on this page. </p>
-              <p class="clr-neutral-80 mb-0"> The following terminology applies to these Terms and Conditions, Privacy Statement and Disclaimer Notice and any or all Agreements: "Client", "You" and "Your" refers to you, the person accessing this website and accepting the Company's terms and conditions. "The Company", "Ourselves", "We", "Our" and "Us", refers to our Company. "Party", "Parties", or "Us", refers to both the Client and ourselves, or either the Client or ourselves. All terms refer to the offer, acceptance and consideration of payment necessary to undertake the process of our assistance to the Client in the most appropriate manner, whether by formal meetings of a fixed duration, or any other means, for the express purpose of meeting the Client's needs in respect of provision of the Company's stated services/products, in accordance with and subject to, prevailing law of . Any use of the above terminology or other words in the singular, plural, capitalisation and/or he/she or they, are taken as interchangeable and therefore as referring to same. </p>
+              <p class="clr-neutral-80 mb-0"> Social Mind is committed to protecting your privacy. This Privacy Policy outlines how we collect,
+                use, and safeguard your information.   </p>
             </div>
             <div class="section-space-xsm-y">
-              <h5 class="clr-neutral-90"> Using our services </h5>
-              <p class="clr-neutral-80"> We will approve link requests from these organizations if we determine that: (a) the link would not reflect unfavorably on us or our accredited businesses (for example, trade associations or other organizations representing inherently suspect types of business, such as work-at-home opportunities, shall not be allowed to link); (b)the organization does not have an unsatisfactory record with us; (c) the benefit to us from the visibility associated with the hyperlink outweighs the absence of Kohost; and (d) where the link is in the context of general resource information or is otherwise consistent with editorial content in a newsletter or similar product furthering the mission of the organization. </p>
-              <p class="clr-neutral-80"> These organizations may link to our home page, to publications or to other Web site information so long as the link: (a) is not in any way misleading; (b) does not falsely imply sponsorship, endorsement or approval of the linking party and it products or services; and (c) fits within the context of the linking party's site. </p>
-              <p class="clr-neutral-80"> If you are among the organizations listed in paragraph 2 above and are interested in linking to our website, you must notify us by sending an e-mail to name@yourdomain.com. Please include your name, your organization name, contact information (such as a phone number and/or e-mail address) as well as the URL of your site, a list of any URLs from which you intend to link to our Web site, and a list of the URL(s) on our site to which you would like to link. Allow 2-3 weeks for a response. </p>
-            </div>
-            <div class="section-space-xsm-y">
-              <h5 class="clr-neutral-90"> Privacy and copyright protection </h5>
-              <p class="clr-neutral-80"> We respond to notices of alleged copyright infringement and terminate accounts of repeat infringers according to the process set out in the U.S. Digital Millennium Copyright Act. </p>
-              <p class="clr-neutral-80"> We provide information to help copyright holders manage their intellectual property online. If you think somebody is violating your copyrights and want to notify us, you can find information about submitting notices and Front's policy about responding to notices in our Help Center. </p>
+              <h5 class="clr-neutral-90"> Information We Collect</h5>
+              <p class="clr-neutral-80"> (a) Name, email address, and other contact information when you register an account. (b) Information on how you use our services, including interactions with social media platforms and analytics. (c) We use cookies to improve user experience and analyze traffic.</p>
             </div>
             <div class="section-space-xsm-y">
               <h5 class="clr-neutral-90"> Your content in our services </h5>
-              <p class="clr-neutral-80"> We reserve the right at any time and in its sole discretion to request that you remove all links or any particular link to our Web site. You agree to immediately remove all links to our Web site upon such request. We also reserve the right to amend these terms and conditions and its linking policy at any time. By continuing to link to our Web site, you agree to be bound to and abide by these linking terms and conditions. </p>
-              <p class="clr-neutral-80"> If you find any link on our Web site or any linked web site objectionable for any reason, you may contact us about this. We will consider requests to remove links but will have no obligation to do so or to respond directly to you. </p>
-              <p class="clr-neutral-80 mb-0"> Whilst we endeavour to ensure that the information on this website is correct, we do not warrant its completeness or accuracy; nor do we commit to ensuring that the website remains available or that the material on the website is kept up to date. </p>
+              <p class="clr-neutral-80"> (a) To provide and maintain our services. (b)  To personalize your experience and improve the functionality of Social Mind. (c) To analyze trends and optimize our platform. </p>
+            </div>
+            <div class="section-space-xsm-y">
+              <h5 class="clr-neutral-90"> Data Protection </h5>
+              <p class="clr-neutral-80">  Social Mind does not sell, trade, or share your personal information with third parties.</p>
+              <p class="clr-neutral-80">  We implement security measures such as data encryption to protect your information from unauthorized access, disclosure, or alteration. However, no transmission over the internet is completely secure</p>
+              <p class="clr-neutral-80 mb-0"> Our platform may link to third-party sites or services. Social Mind is not responsible for the privacy practices or content of these external sites </p>
+              <p class="clr-neutral-80 mb-0"> We retain personal information for as long as your account is active or as needed to provide you services.</p>
+
+            </div>
+            <div class="section-space-xsm-y">
+              <h5 class="clr-neutral-90"> Rights And Privacy Updates </h5>
+              <p class="clr-neutral-80">  You have the right to request access to, correction, or deletion of your personal information. Contact us at privacy@socialmind.com for assistance</p>
+              <p class="clr-neutral-80">  Social Mind reserves the right to update this Privacy Policy. Changes will be posted on this page, and continued use of the site implies acceptance</p>
+              <p class="clr-neutral-80 mb-0"> If you have any questions or concerns about this Privacy Policy, please contact us at privacy@socialmind.com. </p>
+
             </div>
           </div>
         </div>
--- resources/js/components/Terms.vue
+++ resources/js/components/Terms.vue
@@ -31,54 +31,53 @@
           </div>
           <div class="col-xl-8">
             <div class="section-space-xsm-y">
-              <h5 class="clr-neutral-90"> Introduction </h5>
-              <p class="clr-neutral-80"> These terms and conditions outline the rules and regulations for the use of Social Mind's Website. </p>
-              <p class="mb-6 clr-neutral-80"> We're passionate about empowering creatives with the tools they need to bring their vision to life that's why we've developed. Working globally with largest brands, We believe that safe. We combine ideas and behaviours, and insights with that customers love our services. </p>
-              <ul class="list gap-6 mb-6">
-                <li class="welcome-list-1">
-                  <div class="d-flex gap-3">
-                    <span class="flex-shrink-0 d-grid place-content-center w-6 h-6 rounded-circle border border-primary-key border-opacity-3 bg-primary-key bg-opacity-2 clr-primary-key">
-                      <i class="bi bi-check2"></i>
-                    </span>
-                    <span class="d-block clr-neutral-70"> Intelligent image editing </span>
-                  </div>
-                </li>
-                <li class="welcome-list-1">
-                  <div class="d-flex gap-3">
-                    <span class="flex-shrink-0 d-grid place-content-center w-6 h-6 rounded-circle border border-primary-key border-opacity-3 bg-primary-key bg-opacity-2 clr-primary-key">
-                      <i class="bi bi-check2"></i>
-                    </span>
-                    <span class="d-block clr-neutral-70"> Customizable image styles </span>
-                  </div>
-                </li>
-                <li class="welcome-list-1">
-                  <div class="d-flex gap-3">
-                    <span class="flex-shrink-0 d-grid place-content-center w-6 h-6 rounded-circle border border-primary-key border-opacity-3 bg-primary-key bg-opacity-2 clr-primary-key">
-                      <i class="bi bi-check2"></i>
-                    </span>
-                    <span class="d-block clr-neutral-70"> Unprecedented Image Creation </span>
-                  </div>
-                </li>
-              </ul>
-              <p class="clr-neutral-80"> By accessing this website we assume you accept these terms and conditions in full. Do not continue to use Kohost's website if you do not accept all of the terms and conditions stated on this page. </p>
-              <p class="clr-neutral-80 mb-0"> The following terminology applies to these Terms and Conditions, Privacy Statement and Disclaimer Notice and any or all Agreements: "Client", "You" and "Your" refers to you, the person accessing this website and accepting the Company's terms and conditions. "The Company", "Ourselves", "We", "Our" and "Us", refers to our Company. "Party", "Parties", or "Us", refers to both the Client and ourselves, or either the Client or ourselves. All terms refer to the offer, acceptance and consideration of payment necessary to undertake the process of our assistance to the Client in the most appropriate manner, whether by formal meetings of a fixed duration, or any other means, for the express purpose of meeting the Client's needs in respect of provision of the Company's stated services/products, in accordance with and subject to, prevailing law of . Any use of the above terminology or other words in the singular, plural, capitalisation and/or he/she or they, are taken as interchangeable and therefore as referring to same. </p>
+              <h5 class="clr-neutral-90"> Acceptance of Terms </h5>
+              <p class="clr-neutral-80"> By registering for or using Social Mind, you agree to be bound by these Terms and
+                Conditions. If you do not agree, please do not use our services </p>
             </div>
             <div class="section-space-xsm-y">
-              <h5 class="clr-neutral-90"> Using our services </h5>
-              <p class="clr-neutral-80"> We will approve link requests from these organizations if we determine that: (a) the link would not reflect unfavorably on us or our accredited businesses (for example, trade associations or other organizations representing inherently suspect types of business, such as work-at-home opportunities, shall not be allowed to link); (b)the organization does not have an unsatisfactory record with us; (c) the benefit to us from the visibility associated with the hyperlink outweighs the absence of Kohost; and (d) where the link is in the context of general resource information or is otherwise consistent with editorial content in a newsletter or similar product furthering the mission of the organization. </p>
-              <p class="clr-neutral-80"> These organizations may link to our home page, to publications or to other Web site information so long as the link: (a) is not in any way misleading; (b) does not falsely imply sponsorship, endorsement or approval of the linking party and it products or services; and (c) fits within the context of the linking party's site. </p>
-              <p class="clr-neutral-80"> If you are among the organizations listed in paragraph 2 above and are interested in linking to our website, you must notify us by sending an e-mail to name@yourdomain.com. Please include your name, your organization name, contact information (such as a phone number and/or e-mail address) as well as the URL of your site, a list of any URLs from which you intend to link to our Web site, and a list of the URL(s) on our site to which you would like to link. Allow 2-3 weeks for a response. </p>
+              <h5 class="clr-neutral-90">  Use of Services </h5>
+              <p class="clr-neutral-80"> Social Mind provides tools to help you manage social media content, schedule posts,
+                and analyze audience engagement. You agree to use our services responsibly and only
+                for legal purposes. </p>
             </div>
             <div class="section-space-xsm-y">
-              <h5 class="clr-neutral-90"> Privacy and copyright protection </h5>
-              <p class="clr-neutral-80"> We respond to notices of alleged copyright infringement and terminate accounts of repeat infringers according to the process set out in the U.S. Digital Millennium Copyright Act. </p>
-              <p class="clr-neutral-80"> We provide information to help copyright holders manage their intellectual property online. If you think somebody is violating your copyrights and want to notify us, you can find information about submitting notices and Front's policy about responding to notices in our Help Center. </p>
+              <h5 class="clr-neutral-90"> Account Responsibility </h5>
+              <p class="clr-neutral-80">  You are responsible for maintaining the security and confidentiality of your account credentials. Any activity conducted through your account is your responsibility. </p>
             </div>
             <div class="section-space-xsm-y">
-              <h5 class="clr-neutral-90"> Your content in our services </h5>
-              <p class="clr-neutral-80"> We reserve the right at any time and in its sole discretion to request that you remove all links or any particular link to our Web site. You agree to immediately remove all links to our Web site upon such request. We also reserve the right to amend these terms and conditions and its linking policy at any time. By continuing to link to our Web site, you agree to be bound to and abide by these linking terms and conditions. </p>
-              <p class="clr-neutral-80"> If you find any link on our Web site or any linked web site objectionable for any reason, you may contact us about this. We will consider requests to remove links but will have no obligation to do so or to respond directly to you. </p>
-              <p class="clr-neutral-80 mb-0"> Whilst we endeavour to ensure that the information on this website is correct, we do not warrant its completeness or accuracy; nor do we commit to ensuring that the website remains available or that the material on the website is kept up to date. </p>
+              <h5 class="clr-neutral-90"> User Conduct</h5>
+              <p class="clr-neutral-80">  You agree not to misuse the platform for any unlawful or prohibited activity, including unauthorized access, data scraping, or harmful conduct </p>
+            </div>
+            <div class="section-space-xsm-y">
+              <h5 class="clr-neutral-90">  Subscription and Payment</h5>
+              <p class="clr-neutral-80">  Certain features on Social Mind may require a paid subscription. Payment terms will be
+                specified at the time of purchase, and all sales are final unless otherwise specified. </p>
+            </div>
+            <div class="section-space-xsm-y">
+              <h5 class="clr-neutral-90"> Intellectual Property</h5>
+              <p class="clr-neutral-80">  Content generated by users, including any posts, descriptions, tags, or other materials
+                created using Social Mind's tools, remains the intellectual property of the user who
+                created it. Social Mind does not claim ownership of this user-generated content. Users
+                retain all rights and control over this content, including any rights to edit, delete, or
+                distribute it. </p>
+            </div>
+            <div class="section-space-xsm-y">
+              <h5 class="clr-neutral-90"> Limitation of Liability</h5>
+              <p class="clr-neutral-80">  Social Mind shall not be liable for any direct, indirect, incidental, or consequential
+                damages arising from your use or inability to use the service. </p>
+            </div>
+            <div class="section-space-xsm-y">
+              <h5 class="clr-neutral-90">  Termination</h5>
+              <p class="clr-neutral-80">  We reserve the right to suspend or terminate your access to Social Mind at any time if
+                we believe you are in violation of these terms.</p>
+            </div>
+            <div class="section-space-xsm-y">
+              <h5 class="clr-neutral-90"> Changes to Terms</h5>
+              <p class="clr-neutral-80">  Social Mind reserves the right to change or modify these terms at any time. Continued
+                use of the site implies acceptance of these changes.</p>
+                <p class="clr-neutral-80">  For any questions regarding these terms, please contact us at support@socialmind.com.
+                </p>
             </div>
           </div>
         </div>
