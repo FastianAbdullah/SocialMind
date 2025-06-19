126e6f70bbbbb9a4fadadc108e1bb7029db73cf1
M.Abdullah <93811691+FastianAbdullah@users.noreply.github.com>
2025-06-19 17:21:14 +0500

    fix: update Vite configuration to set base path and clean up manifest by removing unused assets

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,8 @@ export default defineConfig(({ command }) => {
     const isProduction = command === 'build';
     
     return {
+        base: './', 
+
         plugins: [
             laravel({
                 input: [
